e4cdabf55b36b4978fdfc1499bf005e2ecd5c841
andywijaya15 <andykevonata@gmail.com>
2022-09-12 15:10:43 +0700

    add userController and loginController

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\LoginRequest;
+
+class LoginController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function __invoke(LoginRequest $request)
+    {
+        $credentials = $request->only('email', 'password');
+
+        if (!$token = auth()->attempt($credentials)) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Email/Password is Wrong'
+            ], 401);
+        }
+
+        return response()->json(
+            compact('token'),
+            200
+        );
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,23 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\UserController;
 
+/**
+ * route "/register"
+ * @method "POST"
+ */
 Route::post('register', RegisterController::class);
+
+/**
+ * route "/login"
+ * @method "POST"
+ */
+Route::post('login', LoginController::class);
+
+/**
+ * route "/user"
+ * @method "GET"
+ */
+Route::middleware('auth:api')->get('user', UserController::class);
