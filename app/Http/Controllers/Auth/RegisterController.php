a44846829fa685c930726f812a68bec232eaae0a
andywijaya15 <andykevonata@gmail.com>
2022-09-12 11:35:42 +0700

    add jwt,FormRequest and Register Controller

---
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Models\User;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\RegisterRequest;
+
+class RegisterController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function __invoke(RegisterRequest $request)
+    {
+        User::create([
+            'name'      => $request->name,
+            'email'     => $request->email,
+            'password'  => bcrypt($request->password)
+        ]);
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Thanks,you are registered.'
+        ], 201);
+    }
+}
